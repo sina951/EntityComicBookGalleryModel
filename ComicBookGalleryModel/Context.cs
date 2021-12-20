88544f5ba83502047e2d7ad89fb5500a4973c882
sinab <sina.behzadi1@gmail.com>
2021-12-20 20:57:47 +0100

    Add project files.

---
+++ ComicBookGalleryModel/Context.cs
@@ -0,0 +1,35 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    public class Context : DbContext
+    {
+        // Our contect class needs to contain a  collection of Db set properties
+        // one propertie for each entity(class) we need to write a query for
+        public Context()
+        {
+            Database.SetInitializer(new DatabaseInitializer());
+        }
+        // set properties
+        public DbSet<ComicBook> ComicBooks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //modelBuilder.Conventions.Remove<DecimalPropertyConvention>();
+            //modelBuilder.Conventions.Add(new DecimalPropertyConvention(5, 2));
+
+            modelBuilder.Entity<ComicBook>()
+                .Property(cb => cb.AverageRating)
+                .HasPrecision(5, 2);
+        }
+    }
+}
---
+++ ComicBookGalleryModel/Program.cs
@@ -0,0 +1,66 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace ComicBookGalleryModel
+{
+    // Here we use the context class to add a comic book, retrieve a list of comic books, and
+    // write the comic book series title to the console
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                context.Database.Log = (message) => Debug.WriteLine(message);
+
+                var comicBookId = 1;
+
+                //var comicBook1 = context.ComicBooks.Find(comicBookId);
+                //var comicBook2 = context.ComicBooks.Find(comicBookId);
+
+                var comicBook1 = context.ComicBooks
+                    .Include(cb => cb.Series)
+                    .Include(cb => cb.Artists.Select(a => a.Artist))
+                    .Include(cb => cb.Artists.Select(a => a.Role))
+                    .SingleOrDefault(cb => cb.Id == comicBookId);
+
+                Debug.WriteLine("Changing the Description property value.");
+                comicBook1.Description = "New value!";
+
+                var comicBook2 = context.ComicBooks
+                    .SingleOrDefault(cb => cb.Id == comicBookId);
+
+                //var comicBooks = context.ComicBooks
+                //    //.Include(cb => cb.Series)
+                //    //.Include(cb => cb.Artists.Select(a => a.Artist))
+                //    //.Include(cb => cb.Artists.Select(a => a.Role))
+                //    .ToList();
+
+                //foreach (var comicBook in comicBooks)
+                //{
+                //    if (comicBook.Series == null)
+                //    {
+                //        context.Entry(comicBook)
+                //            .Reference(cb => cb.Series)
+                //            .Load();
+                //    }
+
+                //    var artistRoleNames = comicBook.Artists
+                //        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
+                //    var artistRolesDisplayText = string.Join(", ", artistRoleNames);
+
+                //    Console.WriteLine(comicBook.DisplayText);
+                //    Console.WriteLine(artistRolesDisplayText);
+                //}
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
