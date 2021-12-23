b1fdd12fe746e1d1a3854c5e3ed3e71168bc4922
sinab <sina.behzadi1@gmail.com>
2021-12-23 23:38:43 +0100

    Final Comments added

--- ComicBookGalleryModel/Program.cs
+++ ComicBookGalleryModel/Program.cs
@@ -23,12 +23,17 @@ namespace ComicBookGalleryModel
             {
                 context.Database.Log = (message) => Debug.WriteLine(message);
 
+                // Details queries
                 var comicBookId = 1;
-
+                // Details queries
                 //var comicBook1 = context.ComicBooks.Find(comicBookId);
                 //var comicBook2 = context.ComicBooks.Find(comicBookId);
 
-                // To get Series data we use Include() method
+                // Lazy Loading
+                // The SingleOrDefault() method will return a single entity or null depending on
+                // whether or not a comicbook is found that matches the provided filter criteria.
+                // if more that one entity is found, the method will throw a exception.
+                // FirstOrDefault() - Returns the first entity if more than one is found
                 var comicBook1 = context.ComicBooks
                     .Include(cb => cb.Series)
                     .Include(cb => cb.Artists.Select(a => a.Artist))
@@ -41,12 +46,14 @@ namespace ComicBookGalleryModel
                 var comicBook2 = context.ComicBooks
                     .SingleOrDefault(cb => cb.Id == comicBookId);
 
+                // Eager Loading - To get Series data we use Include() method - Using Include() means we use Eager Loading
                 //var comicBooks = context.ComicBooks
                 //    //.Include(cb => cb.Series)
                 //    //.Include(cb => cb.Artists.Select(a => a.Artist))
                 //    //.Include(cb => cb.Artists.Select(a => a.Role))
                 //    .ToList();
 
+                // Explicit Loading
                 //foreach (var comicBook in comicBooks)
                 //{
                 //    if (comicBook.Series == null)
